9d4eac3f9b76fe345688f4a9452cb18193414b44
Sayan Chakraborty <jroratorio@gmail.com>
2018-01-27 14:13:26 +0530

    Skeleton structure

---
+++ index.js
@@ -0,0 +1,53 @@
+var weather = require('weather-js');
+var NodeGeocoder = require('node-geocoder');
+var args = require('yargs').argv;
+var config = require('./config.json');
+ 
+var options = {
+  provider: 'google',
+ 
+  httpAdapter: 'https', // Default
+  apiKey: config.googleApi,
+  formatter: null         // 'gpx', 'string', ...
+};
+
+var findTemperatureByCity = function(city, unit) {
+    
+    if(unit !== 'C' || unit !== 'F'){
+        unit === 'F';//setting default unit as F
+    }
+    weather.find({search: city, degreeType: unit}, function(err, result) {
+        if(err) {
+            console.log('Temperature fetch error ' + err.message);
+        } else {
+            console.log('Temperature at ' + result[0].location.name + ' is ' + result[0].current.temperature + " " + result[0].location.degreetype);
+        }  
+    });
+};
+
+var findCityByCoordinates = function( lat, long, unit ){
+    var geocoder = NodeGeocoder(options);
+    geocoder.reverse({lat:lat, lon:long}, function(err, res) {
+        if(err){
+            console.log('Google Geocoder error ' + err.message);
+        }
+        else {
+            var city = res[0].city;
+            findTemperatureByCity( city, unit );
+        }        
+    });    
+};
+
+var findTemperatureByCoordinates = function(lat, long, unit) {
+    findCityByCoordinates(lat, long, unit);
+    
+}
+
+//defaults set for London
+var lat = args.lat || '51.5';
+var long = args.long  || '0.12';
+var unit = args.unit || 'C';
+
+findTemperatureByCoordinates( lat, long, unit);
+
+
---
+++ readme.txt
@@ -0,0 +1,8 @@
+usage:
+node index --lat=[lat] --long=[long] --unit=[unit]
+
+[unit] - F for Fahrenheit, C for celsius
+[lat] - Latitude coordinates upto two decimal places
+[long] - Longitude coordinates upto two decimal places
+
+when no flags are passed, [lat], [long] coordinates default to London.
